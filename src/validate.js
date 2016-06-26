d67a24da2292ec2a07d4be66ee0c0c683355b026
Brian Detering <brian@Brians-MacBook-Pro.local>
2016-06-25 17:48:35 -0700

    loose validation to allow for extension

--- src/validate.js
+++ src/validate.js
@@ -6,7 +6,7 @@ const Validator = require('the_validator');
 const ValidationError = require('./error');
 
 module.exports = (rules, data) => {
-    let errors = new Validator(rules, { strict: true }).test(data);
+    let errors = new Validator(rules, { strict: false }).test(data);
     return _.isEmpty(errors) ?
         Q() : Q.reject(new ValidationError(400, {
             message: 'A validation error occurred',
